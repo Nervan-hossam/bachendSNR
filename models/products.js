664aaa420a08b2f4ecb984be53515b0f95520984
Nervan-hossam <122809318+Nervan-hossam@users.noreply.github.com>
2023-12-08 02:40:40 +0200

    Update products.js

--- models/products.js
+++ models/products.js
@@ -6,7 +6,6 @@ const ProductsSchema = new mongoose.Schema(
     desc : { type : String , required:true }, 
     img : { type : String , required:true }, 
   categories : { type : Array  }, 
-    size : { type : String , required:true }, 
     color : { type : String , required:true }, 
     price : { type : Number , required:true }, 
 
